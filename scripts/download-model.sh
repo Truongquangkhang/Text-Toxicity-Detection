306a91420f2e5db4f731e93cf857547f27c70111
Truongquangkhang <truongqkhang@gmail.com>
2024-06-02 22:20:06 +0700

    update

--- scripts/download-model.sh
+++ scripts/download-model.sh
@@ -1,3 +1,3 @@
-ls
 wget -O ./detection/apis/phoBERT/mymodel.pth --no-check-certificate -r 'https://drive.google.com/uc?export=download&id=1kLzv1ir2tslyB2V-3rim_3tO6z1vGHuZ'
 wget -O ./detection/vinai-phoBERT/model.safetensors --no-check-certificate -r 'https://drive.google.com/uc?export=download&id=1-N4BkNkVuLp6pLE5W01G0PI5fEZAC_Pp'
+ls detection/vinai-phoBERT  
