df4a46f19f7bf526f21f6f88f05254a15f66be17
Truongquangkhang <truongqkhang@gmail.com>
2024-06-02 12:02:17 +0700

    Fix pipeline

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -3,13 +3,12 @@
 readonly image_tag="$1"
 readonly image_repository="$2"
 
-buildDir="detection"
 
 docker build \
   --no-cache \
   --progress=plain \
   --tag "$image_repository":"$image_tag" \
   --file "Dockerfile" \
-  "./$buildDir"
+  "."
 
 docker push "$image_repository":"$image_tag"
