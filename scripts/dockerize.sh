849a35c8a18779f69d3adc081586f9d13fba8c39
Truongquangkhang <truongqkhang@gmail.com>
2024-06-03 21:09:26 +0700

    update install from onedrive

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -3,9 +3,9 @@
 readonly image_tag="$1"
 readonly image_repository="$2"
 
-git lfs pull
+# git lfs pull
 ls -l detection/vinai-phoBERT
-cat detection/vinai-phoBERT/model.safetensors
+# cat detection/vinai-phoBERT/model.safetensors
 
 docker build \
   --no-cache \
---
+++ scripts/download-model.sh
@@ -0,0 +1,7 @@
+# wget -O ./detection/apis/phoBERT/mymodel.pth --no-check-certificate -r 'https://drive.google.com/uc?export=download&id=1kLzv1ir2tslyB2V-3rim_3tO6z1vGHuZ'
+# wget -O ./detection/vinai-phoBERT/model.safetensors --no-check-certificate -r 'https://drive.google.com/uc?export=download&id=1-N4BkNkVuLp6pLE5W01G0PI5fEZAC_Pp'
+# ls detection/vinai-phoBERT
+
+wget -O ./detection/vinai-phoBERT/model.safetensors 'https://dutudn-my.sharepoint.com/:u:/g/personal/102200175_sv1_dut_udn_vn/EWOfVJdhJ8ZCg4T5UMSWV34BdqnDngBKeWneME_3Q5X7zQ?download=1'
+
+wget -O ./detection/apis/phoBERT/mymodel.pth 'https://dutudn-my.sharepoint.com/:u:/g/personal/102200175_sv1_dut_udn_vn/ESUz75Q_wBJBsA2O2q1KtIIB1YfVL6-DuGmi6O8of9tA6Q?download=1'
